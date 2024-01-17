38e5c856faf9da4c9d4f924da4b0aab7f54ce4e0
longschlong1911 <76542115+longschlong1911@users.noreply.github.com>
2024-01-17 16:48:28 -0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -16,6 +16,8 @@ FLAGGED_PHRASES = (
     'getting professional academic help from us is easy',
     'cutt.us',
     'inyurl.com/muxz7h',
+    'to cancer about a week ago',
+    'play station 5',
 )
 
 
@@ -55,8 +57,7 @@ def receive(event, context):
     for phrase in FLAGGED_PHRASES:
         if phrase in message['text'].lower():
             kick_user(message['group_id'], message['user_id'], message['token'])
-            delete_message(message['group_id'], message['id'], message['token'])
-            send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
+            delete_message(message['group_id'], message['id'], message['token'])           
             break
 
     return {
